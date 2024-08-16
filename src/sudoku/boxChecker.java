d11e404f918937f9b0d9490c0aa9d50972511ee3
Nikhil-Kumar02 <120410585+Nikhil-Kumar02@users.noreply.github.com>
2024-08-16 15:36:29 +0530

    Add files via upload

---
+++ src/sudoku/boxChecker.java
@@ -0,0 +1,47 @@
+package sudoku;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class boxChecker extends sudoku implements Runnable{
+    private int startRowbox;
+    private int startColumnbox;
+    private HashMap<Integer,ArrayList<String>> map;
+
+    /**
+     * @param startRowbox
+     * @param startColumnbox 
+     */
+    public boxChecker(int startRowbox, int startColumnbox) {
+        this.startRowbox = startRowbox;
+        this.startColumnbox = startColumnbox;
+        map  = new HashMap<>();
+    }
+    
+    @Override
+    public void run() {
+        checkBox();
+    }
+    
+    private void checkBox() {
+        for (int i = this.startRowbox; i < this.startRowbox + 3; i++) {
+            for (int j = this.startColumnbox; j < this.startColumnbox + 3; j++) {
+                ArrayList<String> indexes;
+                if (this.map.containsKey(this.sudoku[i][j])) {
+                    indexes = this.map.get(this.sudoku[i][j]);
+                } else {
+                    indexes = new ArrayList<>();
+                }
+                indexes.add(i + "," + j);
+                this.map.put(this.sudoku[i][j], indexes);
+            }
+        }
+        for (ArrayList<String> value : this.map.values()) {
+            if (value.size() > 1) {
+                for (String Indexxy : value) {
+                    this.sudokuWrongCells[Integer.parseInt(Indexxy.charAt(0)+"")][Integer.parseInt(Indexxy.charAt(2)+"")] = Boolean.TRUE;
+                }
+            }
+        }
+    }
+}
---
+++ src/sudoku/checker.java
@@ -0,0 +1,49 @@
+package sudoku;
+
+public class checker {
+
+    //Creating objects from checker classes
+    boxChecker box;
+    rowChecker row;
+    columnChecker column;
+
+    /**
+     * @throws InterruptedException
+     */
+    public void check() throws InterruptedException {
+        Thread multiThreadrows[] = new Thread[9];
+        Thread multiThreadcolumns[] = new Thread[9];
+
+        for (int row = 0; row < 9; row++) {
+            this.row = new rowChecker(row);
+            multiThreadrows[row] = new Thread(this.row);
+            multiThreadrows[row].start();
+        }
+
+        for (int column = 0; column < 9; column++) {
+            this.column = new columnChecker(column);
+            multiThreadcolumns[column] = new Thread(this.column);
+            multiThreadcolumns[column].start();
+        }
+
+        int threadId = 0;
+
+        Thread multiThreads[] = new Thread[9];
+        for (int row = 0; row < 9; row += 3) {
+            for (int col = 0; col < 9; col += 3) {
+                this.box = new boxChecker(row, col);
+                multiThreads[threadId] = new Thread(this.box);
+                multiThreads[threadId].start();
+                threadId++;
+            }
+        }
+
+
+        for (int i = 0; i < 9; i++) {
+            multiThreads[i].join();
+            multiThreadrows[i].join();
+            multiThreadcolumns[i].join();
+        }
+
+    }
+}
